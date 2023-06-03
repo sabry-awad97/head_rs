b2766b849b0e159d3cf2918fa16cd3451ff7b089
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-04 00:19:38 +0300

    🐛 fix(src/main.rs): Fix file_path option handling and add stdin support ✨ feat(src/main.rs): Add line number option and improve code structure 🧪 test(cli.rs): Add tests for different scenarios
    The main changes made in this commit are as follows:
    
    In `src/main.rs`:
    - The `file_path` option in the `Cli` struct is now an `Option<PathBuf>` to handle cases when no file path is specified.
    - Added a new `use_stdin` flag to indicate whether to read input from stdin.
    - Added a new `line_numbers` flag to control whether line numbers should be displayed.
    - Refactored the code to extract the logic for reading lines into a separate `read_lines` function to improve code structure and reusability.
    - Modified the `main` function to handle different scenarios based on the options provided.
    
    In `tests/cli.rs`:
    - Renamed the test function `test_head_command` to `test_no_file_or_stdin` to better reflect its purpose.
    - Added new test functions to cover different scenarios, such as reading lines from a file and reading lines from stdin.
    - Updated the assertions in the test functions to match the changes in the code.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Read};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -7,25 +7,51 @@ use structopt::StructOpt;
 #[structopt(name = "head", about = "Print the first N lines of a file.")]
 struct Cli {
     #[structopt(parse(from_os_str))]
-    file_path: PathBuf,
+    file_path: Option<PathBuf>,
 
     #[structopt(short = "n", long = "lines", default_value = "10")]
     num_lines: usize,
+
+    #[structopt(short = "s", long = "stdin")]
+    use_stdin: bool,
+
+    #[structopt(short = "l", long = "line-numbers")]
+    line_numbers: bool,
+}
+
+fn read_lines<R: Read>(reader: R, num_lines: usize, line_numbers: bool) -> io::Result<()> {
+    let reader = BufReader::new(reader);
+    let mut line_count = 0;
+
+    for line in reader.lines() {
+        if line_count >= num_lines {
+            break;
+        }
+        if let Ok(line) = line {
+            if line_numbers {
+                println!("{:>6} {}", line_count + 1, line);
+            } else {
+                println!("{}", line);
+            }
+            line_count += 1;
+        }
+    }
+
+    Ok(())
 }
 
 fn main() -> io::Result<()> {
     let args = Cli::from_args();
-    let file = File::open(args.file_path)?;
-    let reader = BufReader::new(file);
 
-    for (line_number, line) in reader.lines().enumerate() {
-        if line_number >= args.num_lines {
-            break;
-        }
-
-        if let Ok(line) = line {
-            println!("{}", line);
-        }
+    if args.use_stdin {
+        let stdin = io::stdin();
+        let handle = stdin.lock();
+        read_lines(handle, args.num_lines, args.line_numbers)?;
+    } else if let Some(file_path) = args.file_path {
+        let file = File::open(file_path)?;
+        read_lines(file, args.num_lines, args.line_numbers)?;
+    } else {
+        eprintln!("No file or stdin specified.");
     }
 
     Ok(())
--- tests/cli.rs
+++ tests/cli.rs
@@ -8,15 +8,57 @@ type TestResult<T> = Result<T, Box<dyn Error>>;
 const PROGRAM_NAME: &'static str = "head_rs";
 
 #[test]
-fn test_head_command() -> TestResult<()> {
+fn test_no_file_or_stdin() -> TestResult<()> {
     let mut cmd = Command::cargo_bin(PROGRAM_NAME)?;
-    cmd.arg("tests/data/sample.txt").arg("-n").arg("3");
-
-    let expected_output = "1\n2\n3\n";
-
     cmd.assert()
         .success()
-        .stdout(predicate::str::starts_with(expected_output));
+        .stderr(predicate::str::contains("No file or stdin specified."));
+
+    Ok(())
+}
+
+#[test]
+fn test_with_file() -> TestResult<()> {
+    let mut cmd = Command::cargo_bin(PROGRAM_NAME)?;
+    cmd.arg("tests/data/sample.txt")
+        .arg("-n")
+        .arg("3")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("1"))
+        .stdout(predicate::str::contains("2"))
+        .stdout(predicate::str::contains("3"));
+
+    Ok(())
+}
+
+#[test]
+fn test_with_stdin() -> TestResult<()> {
+    let mut cmd = Command::cargo_bin(PROGRAM_NAME)?;
+    cmd.write_stdin("hello\nworld\nfoo\nbar\n")
+        .arg("-n")
+        .arg("2")
+        .arg("-s")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("hello"))
+        .stdout(predicate::str::contains("world"));
+
+    Ok(())
+}
+
+#[test]
+fn test_with_line_numbers() -> TestResult<()> {
+    let mut cmd = Command::cargo_bin(PROGRAM_NAME)?;
+    cmd.arg("tests/data/sample.txt")
+        .arg("-n")
+        .arg("3")
+        .arg("-l")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("1 1"))
+        .stdout(predicate::str::contains("2 2"))
+        .stdout(predicate::str::contains("3 3"));
 
     Ok(())
 }
